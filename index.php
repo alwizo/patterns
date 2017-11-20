0472f4326eff1ba2bc6cb6b896bdd3413a14045d
alwizo <alwizo.com@gmail.com>
2017-11-20 17:11:01 +0100

    Monsters. A simulated monster.

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
 require_once 'vendor/autoload.php';
+
+use Alwizo\Decorator\Monsters\ConcreteMonsters;
+use Alwizo\Decorator\Monsters\ConcreteDecorators;
+
+$monster = new ConcreteMonsters\Zombie();
+$monster = new ConcreteDecorators\ExtremelySmart($monster);
+echo 'My INT is '. $monster->intelligence() . '<br>';
+echo $monster->castSpell('fireball');
