acb3ab0402046d92cc6e574273ee2e16aa83f6fa
alwizo <alwizo.com@gmail.com>
2017-11-20 15:16:52 +0100

    Monsters. Created a superclass for all monsters

---
+++ src/Decorator/Monsters/Monster.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Alwizo\Decorator\Monsters;
+
+
+abstract class Monster
+{
+	public function intelligence(){ return 1; }
+	public function strength(){ return 1; }
+	public function speed(){ return 1; }
+}
